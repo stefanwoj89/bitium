c48bb6d9d69288a270fe35d3cc7e287e2a5c4d65
Stefan Wojciechowski <stefan@sjwcons.com>
2016-05-16 11:37:07 -0700

    basic model design based on spec

--- bit_app/application_check/models.py
+++ bit_app/application_check/models.py
@@ -1,5 +1,12 @@
 from __future__ import unicode_literals
 
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class App(models.Model):
+	name = models.CharField(max_length=200)
+	url = models.URLField()
+
+class AppUser(models.Model):
+	user = models.ForeignKey(User)
+	app = models.ForeignKey(App)
