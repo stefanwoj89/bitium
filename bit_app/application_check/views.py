94b1efd1576baaca0e76c12d622572566e34e20d
Stefan Wojciechowski <stefan@sjwcons.com>
2016-05-16 17:53:36 -0700

    code runs, on ajax state change get response info

--- bit_app/application_check/views.py
+++ bit_app/application_check/views.py
@@ -7,21 +7,33 @@ import json
 def index(request):
 	app_list = []
 	used_apps_list = []
-
+	unused_apps_list = []
 	if request.method == 'GET':
+
 		apps = App.objects.all()
 		for app in apps:
 			app_list.append({'name': app.name, 'urls': [app.url]})
+		return render(request, 'application_check.html', {'apps': json.dumps(app_list)})
+
 	elif request.method == 'POST':
-		app_names = request.POST['names'].split(",")
+		
+		app_names = request.POST['names'].split(",")		
+		unused_apps = App.objects.exclude(name__in=app_names)
+		for unused_app in unused_apps:
+			unused_apps_list.append({'name': unused_app.name})
+
 		for app_name in app_names:
+			if app_name == '':
+				continue
 			app = App.objects.get(name=app_name)
 			au = AppUser.objects.get_or_create(app=app, user=request.user)
-	try:
+
 		used_apps = AppUser.objects.filter(user=request.user)
 		for au in used_apps:
 			used_apps_list.append({'name': au.app.name})
-	except AppUser.DoesNotExist:
-		used_apps = None
+			
+		post_response_data = {}
+		post_response_data['used_apps_list'] = used_apps_list;
+		post_response_data['unused_apps_list'] = unused_apps_list;
 
-	return render(request, 'application_check.html', {'apps': json.dumps(app_list), 'used_apps': json.dumps(used_apps_list)})
+		return HttpResponse(json.dumps(post_response_data), content_type="application/json")
